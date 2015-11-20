721aa0f9d0f08b44f97ff5a95023492870e5d103
Михаил Верещагин <moongomery@yandex.ru>
2015-11-20 19:23:44 +0300

    css fix

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
     var socket = new WebSocket("ws://localhost:3000/message");
 
-    socket.onopen = function() {
-        alert("Соединение установлено.");
-    };
+    //socket.onopen = function() {
+    //    alert("Соединение установлено.");
+    //};
 
-    socket.onclose = function(event) {
-        if (event.wasClean) {
-            alert('Соединение закрыто чисто');
-        } else {
-            alert('Обрыв соединения'); // например, "убит" процесс сервера
-        }
-        alert('Код: ' + event.code + ' причина: ' + event.reason);
-    };
+    //socket.onclose = function(event) {
+    //    if (event.wasClean) {
+    //        alert('Соединение закрыто чисто');
+    //    } else {
+    //        alert('Обрыв соединения'); // например, "убит" процесс сервера
+    //    }
+    //    alert('Код: ' + event.code + ' причина: ' + event.reason);
+    //};
 
     socket.onmessage = function(event) {
         alert("Получены данные " + event.data);
