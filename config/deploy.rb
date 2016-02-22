a3bc58bd7090e69e7d039cff77b5a196c573dc21
Mikkie Cutcopy <moongomery@yandex.ru>
2016-02-23 02:29:25 +0300

    deploy

---
+++ config/deploy.rb
@@ -0,0 +1,125 @@
+# config valid only for current version of Capistrano
+lock '3.4.0'
+
+set :repo_url, 'git@github.com:Mikkie-Cutcopy/artoteka.git'
+
+set :rails_env, 'production'
+
+# Имя пользователя
+set :username, 'mikkie'
+# Имя приложения
+set :application, 'application'
+
+set :deploy_to, "/home/#{fetch(:username)}/#{fetch(:application)}"
+
+set :linked_files, %w{config/secrets.yml config/database.yml}
+set :log_level, :info
+set :unicorn_config, "#{shared_path}/config/unicorn.rb"
+set :unicorn_pid, "#{shared_path}/run/unicorn.pid"
+
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, '2.2.3'
+
+# in case you want to set ruby version from the file:
+# set :rbenv_ruby, File.read('.ruby-version').strip
+
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, '/var/www/my_app_name'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
+# Default value for linked_dirs is []
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+namespace :deploy do
+  after :finishing, 'application:stop'
+  after :finishing, 'application:start'
+  after :finishing, :cleanup
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
+
+namespace :setup do
+  desc 'Загрузка конфигурационных файлов на удаленный сервер'
+  task :upload_config do
+    on roles :all do
+      execute :mkdir, "-p #{shared_path}"
+      ['shared/config', 'shared/run'].each do |f|
+        upload!(f, shared_path, recursive: true)
+      end
+    end
+  end
+end
+
+
+namespace :nginx do
+  desc 'Создание симлинка в /etc/nginx/conf.d на nginx.conf приложения'
+  task :append_config do
+    on roles :all do
+      sudo :ln, "-fs #{shared_path}/config/nginx.conf /etc/nginx/conf.d/#{fetch(:application)}.conf"
+    end
+  end
+  desc 'Релоад nginx'
+  task :reload do
+    on roles :all do
+      sudo :service, :nginx, :reload
+    end
+  end
+  desc 'Рестарт nginx'
+  task :restart do
+    on roles :all do
+      sudo :service, :nginx, :restart
+    end
+  end
+  after :append_config, :restart
+end
+
+
+
+namespace :application do
+  desc 'Запуск Unicorn'
+  task :start do
+    on roles(:app) do
+      execute "cd #{release_path} && ~/.rbenv/shims/bundle exec unicorn_rails -c #{fetch(:unicorn_config)} -E #{fetch(:rails_env)} -D"
+    end
+  end
+  desc 'Завершение Unicorn'
+  task :stop do
+    on roles(:app) do
+      execute "if [ -f #{fetch(:unicorn_pid)} ] && [ -e /proc/$(cat #{fetch(:unicorn_pid)}) ]; then kill -9 `cat #{fetch(:unicorn_pid)}`; fi"
+    end
+  end
+end
+
